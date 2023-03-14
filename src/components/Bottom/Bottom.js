252f28ecb78eb502dc6c6362cc0c3cbf0a815f61
BaptGP <73936647+BaptGP@users.noreply.github.com>
2023-03-14 11:46:37 +0100

    Update Bottom.js

--- src/components/Bottom/Bottom.js
+++ src/components/Bottom/Bottom.js
@@ -1,10 +1,10 @@
 const Bottom = () => {
     return (
-        <header className="h-24 w-screen bg-gray-100 absolute bottom-0 flex items-center">
+        <footer className="h-24 w-screen bg-gray-100 absolute bottom-0 flex items-center">
             <div className="w-full flex justify-center">
                 <a href="http://localhost:3000/">http://localhost:3000/</a>
             </div>
-        </header>
+        </footer>
     )
 }
 
